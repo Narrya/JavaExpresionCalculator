073a57cb7dde0ed128697fa5b8f3c586406f3323
Narrya <kamila.kupidura@gmail.com>
2018-01-05 21:47:31 +0100

    RPN operations

--- src/com/calc/expression/Main.java
+++ src/com/calc/expression/Main.java
@@ -1,6 +1,7 @@
 package com.calc.expression;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,16 +11,42 @@ public class Main {
 		String rpn = keyboard.nextLine();
 		String[] tokens = rpn.split(" ");
 
+		Stack<Double> stack = new Stack<Double>();
+
 		for (String token : tokens) {
 			System.out.println(token);
 
 			try
 			{
 				double number = Double.parseDouble(token);
+				stack.push(number);
 			}
 			catch(NumberFormatException e)
 			{
+				double x = stack.pop();
+				double y = stack.pop();
+
+				switch(token) {
+					case "+":
+						stack.push(y + x);
+						break;
+
+					case "-":
+						stack.push(y - x);
+						break;
+
+					case "*":
+						stack.push(y * x);
+						break;
+
+					case "/":
+						stack.push(y / x);
+						break;
+				}
+
 			}
 		}
+
+		System.out.println(stack.pop());
 	}
 }
